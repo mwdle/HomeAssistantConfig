ea7dc18c3faeb8524759ecfa30ee4ffc03391405
mwdle <mwdle@mwdle.com>
2025-08-20 22:00:47 -0600

    Removed redundant timestamps from router_rebooter pyscript.

--- pyscript/apps/router_rebooter/__init__.py
+++ pyscript/apps/router_rebooter/__init__.py
@@ -1,5 +1,4 @@
 import sys
-from datetime import datetime
 
 # Add the pyscript_modules directory to Python's module search path (`compose.yaml` is configured to mount contents of `python_modules` into container automatically) 
 # See https://hacs-pyscript.readthedocs.io/en/latest/reference.html#importing
@@ -25,23 +24,23 @@ state.persist("pyscript.router_rebooter_status", "Ready", {
 @service
 def reboot_router():
     """Reboots the router."""
-    pyscript.router_rebooter_status = f"[{datetime.now().strftime('%m-%d %H:%M:%S')}] Executing Reboot Command..."
+    pyscript.router_rebooter_status = "Initiating Reboot..."
     # Run blocking Selenium code from module with `task.executor()`
     success, message = task.executor(router_rebooter_module.reboot_router)
     if success:
-        pyscript.router_rebooter_status = f"[{datetime.now().strftime('%m-%d %H:%M:%S')}] Success!"
+        pyscript.router_rebooter_status = "Reboot Triggered!"
     else:
-        pyscript.router_rebooter_status = f"[{datetime.now().strftime('%m-%d %H:%M:%S')}] ERROR | Check Logs!"
+        pyscript.router_rebooter_status = "ERROR | Check Logs!"
         log.error(f"[Router Rebooter]: {message}")
 
 @service
 def reboot_extender():
     """Reboots the extender."""
-    pyscript.extender_rebooter_status = f"[{datetime.now().strftime('%m-%d %H:%M:%S')}] Executing Reboot Command..."
+    pyscript.extender_rebooter_status =  "Initiating Reboot..."
     # Run blocking Selenium code from module with `task.executor()`
     success, message = task.executor(router_rebooter_module.reboot_extender)
     if success:
-        pyscript.extender_rebooter_status = f"[{datetime.now().strftime('%m-%d %H:%M:%S')}] Success!"
+        pyscript.extender_rebooter_status = "Reboot Triggered!"
     else:
-        pyscript.extender_rebooter_status = f"[{datetime.now().strftime('%m-%d %H:%M:%S')}] ERROR | Check Logs!"
+        pyscript.extender_rebooter_status = "ERROR | Check Logs!"
         log.error(f"[Extender Rebooter]: {message}")
